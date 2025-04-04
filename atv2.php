4ea19da6e00c4b67c8cbac1c940740d54851442f
Luiguu <135861824+Luizgustavofidelis@users.noreply.github.com>
2025-04-04 15:49:23 -0300

    Create atv2.php

---
+++ atv2.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>BLOONSTD</title>
+</head>
+
+<body>
+    
+<?php
+    require_once("link.php");
+    
+    
+   function gerar_botao(){
+   echo '<div class="dropdown">';
+   echo '<button class="dropbtn">Times de futebol</button>';
+    echo'<div class="dropText">';
+        echo'<span><img src='.$lista2' width="40" height="40"> MAGOS</span>';
+        echo'<br>';
+        echo'<span><img src="https://i.pinimg.com/originals/f4/23/ca/f423cad8415e2bdfd6de21fce07fd342.png" width="40" height="40">HEROIS</span>';
+        echo'<br>';
+        echo'<span><img src="https://wallpaper.forfun.com/fetch/e8/e8e0477990ca2870d749ccd166679316.jpeg" width="40" height="40">PRIMARIOS</span>';
+        echo'<br>';
+    echo'</div>';
+echo'</div>';
+
+    
+   }
+    
+    $lista2 = array();
+    $link2 = new Link();
+    $link2->setLinkimg("https://bk.ibxk.com.br/2023/08/28/28110121537005.png");
+    $link2->setInfo("domador");
+    array_push($lista2,$link2);
+    $link2 = new Link();
+    $link2->setLinkimg("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNzGmgdlsuwTMNsauditTHtUaMimL7LckwIg&s");
+    $link2->setInfo("macaco ninja");
+    array_push($lista2,$link2);
+    $link2 = new Link();
+    $link2->setLinkimg("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeYOAXumIFZRWCAfS6Vf4Imi2kKGtaph_z3g&s");
+    $link2->setInfo("macaco 10 anos");
+    array_push($lista2,$link2);
+
+   foreach($LISTA)
+
+    $lista3 = array();
+    $link3 = new Link();
+    $link3->setLinkimg("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSK7Vc8vQ9hqDUa7kjAXhtec3E3sK7ZHgRUA&s");
+    $link3->setInfo("macaco forte");
+    array_push($lista3,$link3);
+    $link3 = new Link();
+    $link3->setLinkimg("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwK1UNvI2VP3HWwusXo4gSsIpM0Ai1x78odA&s");
+    $link3->setInfo("macacobumetrange");
+    array_push($lista3,$link3);
+    $link3 = new Link();
+    $link3->setLinkimg("https://static.wikia.nocookie.net/b__/images/9/99/000-WizardMonkey.png/revision/latest?cb=20190522015102&path-prefix=bloons");
+    $link3->setInfo("macaco mago");
+    array_push($lista3,$link3);
+
+    $lista3 = gerar_botao();
+
+    $lista4 = array();
+    $link4 = new Link();
+    $link4->setLinkimg("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4CPD90aEkiLucwPBNOUaXkL8NQYATfMD0kA&s");
+    $link4->setInfo("macacomassa");
+    array_push($lista4,$link4);
+    $link4 = new Link();
+    $link4->setLinkimg("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4CPD90aEkiLucwPBNOUaXkL8NQYATfMD0kA&s");
+    $link4->setInfo("macacolegal");
+    array_push($lista4,$link4);
+    $link4 = new Link();
+    $link4->setLinkimg("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4CPD90aEkiLucwPBNOUaXkL8NQYATfMD0kA&s");
+    $link4->setInfo("macaconaoegal");
+    array_push($lista4,$link4);
+
+    $lista4 = gerar_botao();
+    
+
+    
+    ?>
+
+<div class="dropdown">
+        <button class="dropbtn">Times de futebol</button>
+        <div class="dropText">
+            <span><img src="https://i.pinimg.com/736x/f2/b1/fc/f2b1fc8ac8c1273f51a8385ac3fe8514.jpg" width="40" height="40"> MAGOS</span>
+            <br>
+            <span><img src="https://i.pinimg.com/originals/f4/23/ca/f423cad8415e2bdfd6de21fce07fd342.png" width="40" height="40">HEROIS</span>
+            <br>
+            <span><img src="https://wallpaper.forfun.com/fetch/e8/e8e0477990ca2870d749ccd166679316.jpeg" width="40" height="40">PRIMARIOS</span>
+            <br>
+        </div>
+    </div>
+</body>
+
+</html>
